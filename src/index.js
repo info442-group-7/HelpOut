a7c4b97660a4faa0f45e708c0230561239e26072
Jamie Walker <jamiekw@uw.edu>
2020-05-25 22:10:29 -0700

    added firebase init, components

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { DatePicker, message } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 
 // const App = () => {
 //   const [date, setDate] = useState(null);
@@ -22,6 +26,21 @@ import * as serviceWorker from './serviceWorker';
 //     </div>
 //   );
 // };
+const firebaseConfig = {
+    apiKey: "AIzaSyCjJR5NV13iDYluMYu-EQQZMIlZ2DwhW9A",
+    authDomain: "help-out-info-442.firebaseapp.com",
+    databaseURL: "https://help-out-info-442.firebaseio.com",
+    projectId: "help-out-info-442",
+    storageBucket: "help-out-info-442.appspot.com",
+    messagingSenderId: "487992417747",
+    appId: "1:487992417747:web:af6e6e755c8a62f3c4e326",
+    measurementId: "G-2YLQWRCY46"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
