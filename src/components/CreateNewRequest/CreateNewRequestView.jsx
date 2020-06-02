cba4a863d9e43a6786dcf9dab54cd6e49b1d866f
Jamie Walker <jamiekw@uw.edu>
2020-06-02 10:40:54 -0700

    create new request connected to db

--- src/components/CreateNewRequest/CreateNewRequestView.jsx
+++ src/components/CreateNewRequest/CreateNewRequestView.jsx
@@ -2,7 +2,7 @@ import { Form, Input, InputNumber, Modal, Button } from 'antd';
 import React, { Component } from 'react';
 import { CheckCircleOutlined } from '@ant-design/icons';
 import { Redirect, Link, useHistory } from 'react-router-dom';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 
 const { confirm } = Modal;
 
@@ -36,10 +36,42 @@ const CreateNewRequestView = () => {
   // on successful submission, creates modal 
   const onFinish = values => {
     console.log('Success:', values);
+    console.log(values.zip)
     // let userTest = firebase.database().ref("USER");
     // userTest.push({user_city: "TESTING"})
 
     // somehow send to database here!
+
+  
+
+// src: https://stackoverflow.com/questions/38768576/in-firebase-when-using-push-how-do-i-get-the-unique-id-and-store-in-my-databas
+
+    let requestRef = firebase.database().ref("REQUEST").push();
+    var key = requestRef.key;
+
+
+    var newRequestData={
+      REQUEST_ID: key,
+      REQUESTER_ID: 'test', //eventually put userid = firebase user auth id
+      REQUEST_DATE: new Date().toLocaleString(),
+      REQUEST_DESCRIPTION: values.details,
+      REQUEST_STATUS: "incomplete", // to start
+      REQUEST_TIME: 'mock - add date function here',
+      REQUEST_TITLE: values.title,
+      USER_ID: '' //ONCE SOMEONe accepts, this is updated
+
+    }
+
+    // requestRef.push({
+    //   REQUESTER_ID: 'test', //eventually put userid = firebase user auth id
+    //   REQUEST_DATE: new Date().toLocaleString(),
+    //   REQUEST_DESCRIPTION: values.details,
+
+    // })
+
+    requestRef.push(newRequestData)
+
+    
 
     success();
   };
@@ -70,19 +102,19 @@ const CreateNewRequestView = () => {
     <div>
       <h2>Create a new request</h2>
       <Form id="create-request" {...layout} style={{width:800}} name="nest-messages" onFinish={onFinish} onFinishFailed={onFinishFailed} validateMessages={validateMessages}>
-        <Form.Item name={['user', 'name']} label="I need help with " rules={[{ required: true, message:'Your request title is required!' }]}>
+        <Form.Item name={['title']} label="I need help with " rules={[{ required: true, message:'Your request title is required!' }]}>
           <Input style={{width: 500}}/>
         </Form.Item>
-        <Form.Item name={['user', 'email']} label="Could you give additional details?">
+        <Form.Item name={['details']} label="Could you give additional details?">
           <Input.TextArea style={{width: 500}} />
         </Form.Item>
         {/* <Form.Item name={['user', 'age']} label="This task can be categorized as" rules={[{ type: 'number', min: 0, max: 99 }]}>
           <InputNumber />
         </Form.Item> */}
-        <Form.Item name={['user', 'address']} label="My address is" rules={[{ required: true, message:'Address is required!'}]}>
+        <Form.Item name={['address']} label="My address is" rules={[{ required: true, message:'Address is required!'}]}>
           <Input style={{ width: 500 }} />
         </Form.Item>
-        <Form.Item name={['user', 'zip']} label="Zipcode" maxLength='5' rules={[{ required: true }]}>
+        <Form.Item name={['zip']} label="Zipcode" maxLength='5' rules={[{ required: true }]}>
           <Input style={{ width: 100, textAlign: 'center' }} maxLength="5" placeholder="12345" />
 
         </Form.Item>
