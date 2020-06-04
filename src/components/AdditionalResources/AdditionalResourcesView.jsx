7632ed848202ef357369ca67b983f200ae8f3728
pranita122 <35115093+pranita122@users.noreply.github.com>
2020-06-03 18:30:20 -0700

    add additional resources

--- src/components/AdditionalResources/AdditionalResourcesView.jsx
+++ src/components/AdditionalResources/AdditionalResourcesView.jsx
@@ -26,6 +26,39 @@ const AdditionalResourcesView = (props) => {
                     fontWeight: 'bolder'
                 }} a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019">WHO Resources 
                 </Button>
+                <Button className="bar-button" type="primary" style={{
+                    fontSize: '3vmin',
+                    marginTop: '1vmin',
+                    paddingBottom: '6vmin',
+                    paddingTop: '2vmin',
+                    width: '110%',
+                    textAlign: 'center',
+                    borderRadius: '5px',
+                    fontWeight: 'bolder'
+                }} a href="https://www.usda.gov/coronavirus"> USDA Resources
+                </Button>
+                <Button className="bar-button" type="primary" style={{
+                    fontSize: '3vmin',
+                    marginTop: '1vmin',
+                    paddingBottom: '6vmin',
+                    paddingTop: '2vmin',
+                    width: '110%',
+                    textAlign: 'center',
+                    borderRadius: '5px',
+                    fontWeight: 'bolder'
+                }} a href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"> CDC Resources
+                </Button>
+                <Button className="bar-button" type="primary" style={{
+                    fontSize: '3vmin',
+                    marginTop: '1vmin',
+                    paddingBottom: '6vmin',
+                    paddingTop: '2vmin',
+                    width: '110%',
+                    textAlign: 'center',
+                    borderRadius: '5px',
+                    fontWeight: 'bolder'
+                }} a href="https://kingcounty.gov/depts/health/communicable-diseases/disease-control/novel-coronavirus.aspx"> King County Resources
+                </Button>
             </div>
         </div>
         </div>
